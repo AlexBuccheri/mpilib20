e219006784ee543528d6c555ae06bc190c2fb8cc
Max Williams <me@maxwillia.ms>
2021-02-15 08:57:19 +0000

    The root argument of mpilib20_reduce_<T>() generics is optional, allowing overiding of the default master rank within mpilib20_init_finalise.

--- src/routines/mpilib20_reduce.F90
+++ src/routines/mpilib20_reduce.F90
@@ -21,41 +21,58 @@ module mpilib20_reduce_m
 contains
 
     !> MPI_REDUCE WRAPPER: INTEGER
-    subroutine mpilib20_reduce_int_scalar(sendbuf, recvbuf, operation, root, mpi_env)
+    subroutine mpilib20_reduce_int_scalar(sendbuf, recvbuf, operation, mpi_env, root)
         use mpi_bindings, only : MPI_REDUCE, MPI_Op, &
             MPI_Datatype, MPI_INTEGER
+        use mpilib20_init_finalise, only : root_id
         !> Variable containing set to be sent
         type(MPI_Datatype),          intent(in)      :: sendbuf
         !> Variable to receive reduced set
         type(MPI_Datatype),          intent(inout)   :: recvbuf
         !> MPI Operation
-        type(MPI_Op),       intent(in)      :: operation
-        !> Rank of the process to receive the reduced set
-        integer,            intent(in)      :: root
+        type(MPI_Op),                intent(in)      :: operation
         !> Instance of the MPI environment
-        type(mpi_env_type), intent(inout)   :: mpi_env
+        type(mpi_env_type),          intent(inout)   :: mpi_env
+        !> Rank of the process to receive the reduced set (override default)
+        integer, optional,           intent(in)      :: root
+        integer                                      :: use_root
 
-        call MPI_REDUCE(sendbuf, recvbuf, 1, MPI_INTEGER, operation, root, mpi_env%comm, mpi_env%ierror)
+        !> Overide root process if passed
+        if (present(root)) then
+            use_root = root
+        else
+            use_root = root_id
+        end if
+
+        call MPI_REDUCE(sendbuf, recvbuf, 1, MPI_INTEGER, operation, use_root, mpi_env%comm, mpi_env%ierror)
 
     end subroutine mpilib20_reduce_int_scalar
 
     !> MPI_REDUCE WRAPPER: INTEGER(:)
     !> Rank 1 arrays don't require flattening
-    subroutine mpilib20_reduce_int_vec(sendbuf, recvbuf, operation, root, mpi_env)
+    subroutine mpilib20_reduce_int_vec(sendbuf, recvbuf, operation, mpi_env, root)
         use mpi_bindings, only : MPI_REDUCE, MPI_Op, &
             MPI_Datatype, MPI_INTEGER
+        use mpilib20_init_finalise, only : root_id
         !> Variable containing set to be sent
-        type(MPI_Datatype), intent(in)               :: sendbuf(:)
+        type(MPI_Datatype),         intent(in)      :: sendbuf(:)
         !> Variable to receive reduced set
-        type(MPI_Datatype), intent(inout)        :: recvbuf(:)
+        type(MPI_Datatype),         intent(inout)   :: recvbuf(:)
         !> MPI Operation
         type(MPI_Op),               intent(in)      :: operation
-        !> Rank of the process to receive the reduced set
-        integer,                    intent(in)      :: root
         !> Instance of the MPI environment
         type(mpi_env_type),         intent(inout)   :: mpi_env
-        !> elements in each buffer
+        !> Rank of the process to receive the reduced set (override default)
+        integer, optional,          intent(in)      :: root
         integer                                     :: count
+        integer                                     :: use_root
+
+        !> Overide root process if passed
+        if (present(root)) then
+            use_root = root
+        else
+            use_root = root_id
+        end if
         !> Element count based-on receiving buffer size
         count = size(recvbuf)
 
