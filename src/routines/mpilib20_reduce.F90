ecd0bf742eb7643c1ff6db1c5264d71867d56ab3
Max Williams <me@maxwillia.ms>
2021-02-20 17:48:21 +0000

    PR changes
    - remove generic_template file - useful locally if useful during development
    - mpilib20_REDUCE interface -> mpilib20_reduce
    - global imports moved to module scope
    - recvbuf (receiving buffer) for integer case -> intent(out)
    - use_rank (integer) renamed to process_id. Persistent rank is then renamed to process in calls.
    - reduce verbosity when inferring dimension of buffers. Use size(recvbuf) rather than storing in `count` variable.
    - add assert to check that sendbuf and recvbuf are equal dimension
    - update todos at module docs (remaining types etc)
    - update todos at subroutine level: usage examples

--- src/routines/mpilib20_reduce.F90
+++ src/routines/mpilib20_reduce.F90
@@ -1,11 +1,17 @@
 !> Wrapping of the MPI_REDUCE methods
 !> TODOs
-!> Unravel how MPI_Datatype operates
-!> Overload for MPI_IN_PLACE?
+!> Up to rank 3 arrays
+!> Remaining types (parametized derived types for precisions?)
+!>  real(sp), real(dp), real(qp)
+!>  complex
+!> Overload for MPI_IN_PLACE - optional logical could work
 !> Testing
+!> Refactor when root_id moved to mpi_env_type
 module mpilib20_reduce_m
 
-    use mpilib20_init_finalise, only : mpi_env_type
+    use mpilib20_init_finalise, only : mpi_env_type, root_id
+    use mpi_bindings, only : MPI_REDUCE, MPI_Op, &
+        MPI_INTEGER
 
     implicit none
 
@@ -16,68 +22,73 @@ module mpilib20_reduce_m
         module procedure mpilib20_reduce_int_vec
     end interface
     
-    public :: mpilib20_REDUCE
+    public :: mpilib20_reduce
     
 contains
 
-    !> MPI_REDUCE WRAPPER: INTEGER
-    subroutine mpilib20_reduce_int_scalar(sendbuf, recvbuf, operation, mpi_env, root)
-        use mpi_bindings, only : MPI_REDUCE, MPI_Op, &
-            MPI_Datatype, MPI_INTEGER
-        use mpilib20_init_finalise, only : root_id
+    
+    !> Reduces values on all processes to a single value
+    !> on process defined by process_id
+    !> TODO usage examples
+    subroutine mpilib20_reduce_int_scalar(sendbuf, recvbuf, &
+                                          operation, mpi_env, &
+                                          process_id)
         !> Variable containing set to be sent
-        type(MPI_Datatype),          intent(in)      :: sendbuf
+        type(MPI_INTEGER),          intent(in)      :: sendbuf
         !> Variable to receive reduced set
-        type(MPI_Datatype),          intent(inout)   :: recvbuf
+        type(MPI_INTEGER),          intent(out)     :: recvbuf
         !> MPI Operation
-        type(MPI_Op),                intent(in)      :: operation
+        type(MPI_Op),               intent(in)      :: operation
         !> Instance of the MPI environment
-        type(mpi_env_type),          intent(inout)   :: mpi_env
+        type(mpi_env_type),         intent(out)     :: mpi_env
         !> Rank of the process to receive the reduced set (override default)
-        integer, optional,           intent(in)      :: root
-        integer                                      :: use_root
+        integer, optional,          intent(in)      :: process_id
+        integer                                     :: process
 
         !> Overide root process if passed
-        if (present(root)) then
-            use_root = root
+        if (present(process_id)) then
+            process = process_id
         else
-            use_root = root_id
+            process = root_id
         end if
 
-        call MPI_REDUCE(sendbuf, recvbuf, 1, MPI_INTEGER, operation, use_root, mpi_env%comm, mpi_env%ierror)
+        call MPI_REDUCE(sendbuf, recvbuf, 1, MPI_INTEGER, operation, process, &
+            mpi_env%comm, mpi_env%ierror)
 
     end subroutine mpilib20_reduce_int_scalar
 
-    !> MPI_REDUCE WRAPPER: INTEGER(:)
-    !> Rank 1 arrays don't require flattening
-    subroutine mpilib20_reduce_int_vec(sendbuf, recvbuf, operation, mpi_env, root)
-        use mpi_bindings, only : MPI_REDUCE, MPI_Op, &
-            MPI_Datatype, MPI_INTEGER
-        use mpilib20_init_finalise, only : root_id
+
+    !> Reduces values on all processes to a single value
+    !> on process defined by process_id
+    !> TODO usage examples
+    subroutine mpilib20_reduce_int_vec(sendbuf, recvbuf, &
+                                       operation, mpi_env, & 
+                                       process_id)
         !> Variable containing set to be sent
-        type(MPI_Datatype),         intent(in)      :: sendbuf(:)
+        type(MPI_INTEGER),         intent(in)      :: sendbuf(:)
         !> Variable to receive reduced set
-        type(MPI_Datatype),         intent(inout)   :: recvbuf(:)
+        type(MPI_INTEGER),         intent(inout)   :: recvbuf(:)
         !> MPI Operation
         type(MPI_Op),               intent(in)      :: operation
         !> Instance of the MPI environment
         type(mpi_env_type),         intent(inout)   :: mpi_env
         !> Rank of the process to receive the reduced set (override default)
-        integer, optional,          intent(in)      :: root
-        integer                                     :: count
-        integer                                     :: use_root
+        integer, optional,          intent(in)      :: process_id
+        integer                                     :: process
 
         !> Overide root process if passed
-        if (present(root)) then
-            use_root = root
+        if (present(process_id)) then
+            process = process_id
         else
-            use_root = root_id
+            process = root_id
         end if
-        !> Element count based-on receiving buffer size
-        count = size(recvbuf)
 
-        call MPI_REDUCE(sendbuf, recvbuf, count, MPI_INTEGER, operation, root, mpi_env%comm, mpi_env%ierror)
+        call assert(size(sendbuf) == size(recvbuf), "size(sendbuf) /= size(recvbuf)")
+
+        call MPI_REDUCE(sendbuf, recvbuf, size(recvbuf), MPI_INTEGER, operation, process, &
+        mpi_env%comm, mpi_env%ierror)
 
     end subroutine mpilib20_reduce_int_vec
+
 
 end module mpilib20_reduce_m
